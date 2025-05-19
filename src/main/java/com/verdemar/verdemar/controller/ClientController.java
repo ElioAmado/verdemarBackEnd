ebe939d0f0386ce645d2efe676f2805eef1cd552
ElioAmado <amadocosta5@gmail.com>
2025-05-19 07:59:30 +0200

    Refactor ClientController to improve import organization and use constructor injection for ClientService

--- src/main/java/com/verdemar/verdemar/controller/ClientController.java
+++ src/main/java/com/verdemar/verdemar/controller/ClientController.java
@@ -1,23 +1,27 @@
 package com.verdemar.verdemar.controller;
 
-import com.verdemar.verdemar.domain.Client;
-import com.verdemar.verdemar.service.ClientService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.verdemar.verdemar.domain.Client;
+import com.verdemar.verdemar.service.ClientService;
 
 @RestController
 @RequestMapping("/api/clients")
 public class ClientController {
 
-    private final ClientService clientService;
-
     @Autowired
-    public ClientController(ClientService clientService) {
-        this.clientService = clientService;
-    }
+    private ClientService clientService;
 
     // Obtener todos los clientes
     @GetMapping
