1da4a1dd572cb78e55e8ec0e3ce1b16cdb10ec80
Elio <amadocosta5@gmail.com>
2025-10-15 19:20:11 +0200

    Comment

--- src/main/java/com/verdemar/controller/PriceController.java
+++ src/main/java/com/verdemar/controller/PriceController.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -63,12 +64,14 @@ public class PriceController {
     return ResponseEntity.ok(updatedPrice);
   }
 
+  // Deletes
+
   // Eliminar un precio
-  // @DeleteMapping("/{apartment}/{date}")
-  // public ResponseEntity<Void> deletePrice(
-  // @PathVariable Short apartment,
-  // @PathVariable LocalDate date) {
-  // priceService.deletePrice(apartment, date);
-  // return ResponseEntity.noContent().build();
-  // }
+   @DeleteMapping("/{apartment}/{date}")
+   public ResponseEntity<Void> deletePrice(
+   @PathVariable Short apartment,
+   @PathVariable LocalDate date) {
+   priceService.deletePrice(apartment, date);
+   return ResponseEntity.noContent().build();
+   }
 }
