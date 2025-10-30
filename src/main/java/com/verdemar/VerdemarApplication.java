4a1a4838ceee50626d03369e13ad5991cef3481f
Elio Amado Costa <amadocosta5@gmail.com>
2025-10-30 09:54:32 +0100

    All ok

--- src/main/java/com/verdemar/VerdemarApplication.java
+++ src/main/java/com/verdemar/VerdemarApplication.java
@@ -1,12 +1,21 @@
 package com.verdemar;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @SpringBootApplication
 public class VerdemarApplication {
 
+  @Autowired
+  private static Logger logger = LogManager.getLogger(VerdemarApplication.class);
   public static void main(String[] args) {
+    logger.info("Arrancando la frutería 🍎");
+    logger.debug("Modo debug activado");
+    logger.error("¡Algo ha fallado con las naranjas!");
     SpringApplication.run(VerdemarApplication.class, args);
   }
 }
